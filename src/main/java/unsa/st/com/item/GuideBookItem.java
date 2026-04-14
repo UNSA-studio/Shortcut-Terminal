810d9f363511d5b658d51cb61cc13a46df37f82a
UNSA-STUDIO <UNSA-studio@outlook.com>
2026-04-14 22:00:55 +0800

    Fix GuideBookItem using correct DataComponents API for 1.21.1

--- src/main/java/unsa/st/com/item/GuideBookItem.java
+++ src/main/java/unsa/st/com/item/GuideBookItem.java
@@ -1,9 +1,6 @@
 package unsa.st.com.item;
 
 import net.minecraft.core.component.DataComponents;
-import net.minecraft.nbt.CompoundTag;
-import net.minecraft.nbt.ListTag;
-import net.minecraft.nbt.StringTag;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
@@ -12,6 +9,11 @@ import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.WrittenBookItem;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.item.component.WrittenBookContent;
+import net.minecraft.util.Filterable;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class GuideBookItem extends WrittenBookItem {
     public GuideBookItem(Properties properties) {
@@ -29,40 +31,47 @@ public class GuideBookItem extends WrittenBookItem {
     }
 
     private void setupBookContent(ItemStack stack) {
-        // 通过 NBT 设置书本内容
-        CompoundTag tag = stack.getOrCreateTag();
-        tag.putString("author", "UNSA-STUDIO");
-        tag.putString("title", Component.translatable("st.guide.title").getString());
-        
-        ListTag pages = new ListTag();
-        pages.add(StringTag.valueOf(Component.literal(
-            "=== Shortcut Terminal ===\n\n" +
-            "Welcome to Shortcut Terminal!\n\n" +
-            "This guide contains all commands.\n\n" +
-            "Use /ST Help in chat to see them."
-        ).getString()));
-        pages.add(StringTag.valueOf(Component.literal(
-            "/ST ls - List files\n" +
-            "/ST mkdir <name> - Create directory\n" +
-            "/ST touch <name> - Create file\n" +
-            "/ST rm <name> - Remove file\n" +
-            "/ST cat <name> - View file\n" +
-            "/ST cd <path> - Change directory\n" +
-            "/ST pwd - Show current path\n" +
-            "/ST whoami - Show username\n" +
-            "/ST clear - Clear screen\n" +
-            "/ST refresh bf - Refresh plugins"
-        ).getString()));
-        pages.add(StringTag.valueOf(Component.literal(
-            "/ST User <player> <action> [params]\n\n" +
-            "Actions:\n" +
-            "- switchingmode <mode>\n" +
-            "- changebirthpoint\n" +
-            "- transmitto_online <x y z|home>\n" +
-            "- transmitto_offline <x y z|home>\n\n" +
-            "Example:\n" +
-            "/ST User Steve switchingmode creative"
-        ).getString()));
-        tag.put("pages", pages);
+        // 构建书本内容
+        WrittenBookContent content = new WrittenBookContent(
+            Filterable.passThrough("Shortcut Terminal Guide"),
+            Filterable.passThrough("UNSA-STUDIO"),
+            0, // generation
+            createPages(),
+            true // resolved
+        );
+        stack.set(DataComponents.WRITTEN_BOOK_CONTENT, content);
+    }
+
+    private List<Filterable<Component>> createPages() {
+        return List.of(
+            Filterable.passThrough(Component.literal(
+                "=== Shortcut Terminal ===\n\n" +
+                "Welcome to Shortcut Terminal!\n\n" +
+                "This guide contains all commands.\n\n" +
+                "Use /ST Help in chat to see them."
+            )),
+            Filterable.passThrough(Component.literal(
+                "/ST ls - List files\n" +
+                "/ST mkdir <name> - Create directory\n" +
+                "/ST touch <name> - Create file\n" +
+                "/ST rm <name> - Remove file\n" +
+                "/ST cat <name> - View file\n" +
+                "/ST cd <path> - Change directory\n" +
+                "/ST pwd - Show current path\n" +
+                "/ST whoami - Show username\n" +
+                "/ST clear - Clear screen\n" +
+                "/ST refresh bf - Refresh plugins"
+            )),
+            Filterable.passThrough(Component.literal(
+                "/ST User <player> <action> [params]\n\n" +
+                "Actions:\n" +
+                "- switchingmode <mode>\n" +
+                "- changebirthpoint\n" +
+                "- transmitto_online <x y z|home>\n" +
+                "- transmitto_offline <x y z|home>\n\n" +
+                "Example:\n" +
+                "/ST User Steve switchingmode creative"
+            ))
+        );
     }
 }
